c978259b6219f18497e118801455520039361cbb
elementz <33937944+elementzprojects@users.noreply.github.com>
2018-09-29 08:46:34 -0400

    Update mathlib.py

--- mathlib.py
+++ mathlib.py
@@ -1,31 +1,31 @@
 def add(a, b):
-    return a + b
+    return str(a + b)
 
 def subtract(a, b):
-    return a - b
+    return str(a - b)
 
 def multiply(a, b):
-    return a * b
+    return str(a * b)
 
 def divide(a, b):
-    return a / b
+    return str(a / b)
 
 def square_area(length, width):
-    return length * width
+    return str(length * width)
 
 def circle_area(radius):
-    return (3.14 * radius^2)
+    return str((3.14 * radius^2))
 
 def triangle_area(length, base):
-    return (length * base / 2)
+    return str((length * base / 2))
 
 def rectangle_perimeter(length, width):
-    return 2(length + width)
+    return str(2(length + width))
 
 
 def square_perimeter(side):
-    return 4 * side
+    return str(4 * side)
 
 
 def exponents(number,exponent):
-    return number^exponent
+    return str(number^exponent)
