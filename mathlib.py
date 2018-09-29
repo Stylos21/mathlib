071214ecd95e9ccf151bce77d059306b92ce2269
elementz <bfdifan21@gmail.com>
2018-09-29 08:29:34 -0400

    first commit

---
+++ mathlib.py
@@ -0,0 +1,31 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+def square_area(length, width):
+    return length * width
+
+def circle_area(radius):
+    return (3.14 * radius^2)
+
+def triangle_area(length, base):
+    return (length * base / 2)
+
+def rectangle_perimeter(length, width):
+    return 2(length + width)
+
+
+def square_perimeter(side):
+    return 4 * side
+
+
+def exponents(number,exponent):
+    return number^exponent
